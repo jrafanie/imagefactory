c241b0e595def824712fdd9cd9bbc9c4eb884810
Steve Loranz <sloranz@redhat.com>
2014-04-28 13:47:47 -0500

    misc fixes

--- imagefactory_plugins/Nova/Nova.py
+++ imagefactory_plugins/Nova/Nova.py
@@ -18,6 +18,7 @@ import zope
 from imgfac.ApplicationConfiguration import ApplicationConfiguration
 from imgfac.OSDelegate import OSDelegate
 from novaimagebuilder.Builder import Builder as NIB
+from novaimagebuilder.StackEnvironment import StackEnvironment
 
 
 class Nova(object):
@@ -27,27 +28,25 @@ class Nova(object):
         super(Nova, self).__init__()
         self.app_config = ApplicationConfiguration().configuration
         self.log = logging.getLogger('%s.%s' % (__name__, self.__class__.__name__))
-        self.tdlobj = None
-        self.cloud_plugin_content = []
-        self.parameters = None
-        self.builder = None
         self.nib = None
 
     def abort(self):
-        status = self.nib.abort()
-        self.log.debug('aborting... status: %s' % status)
+        if self.nib and isinstance(self.nib, NIB):
+            status = self.nib.abort()
+            self.log.debug('aborting... status: %s' % status)
+        else:
+            self.log.debug('No active Nova Image Builder instance found, nothing to abort.')
 
     def create_base_image(self, builder, template, parameters):
         self.log.info('create_base_image() called for Nova plugin - creating a BaseImage')
 
-        self.builder = builder
         self.log.debug('builder set to %s' % builder)
-        self.parameters = parameters if parameters else {}
+        parameters = parameters if parameters else {}
         self.log.debug('parameters set to %s' % parameters)
 
         # Derive the OSInfo OS short_id from the os_name and os_version in template
-        if(template.os_version):
-            if(template.os_name[-1].isdigit()):
+        if template.os_version:
+            if template.os_name[-1].isdigit():
                 install_os = '%s.%s' % (template.os_name, template.os_version)
             else:
                 install_os = '%s%s' % (template.os_name, template.os_version)
@@ -55,8 +54,8 @@ class Nova(object):
             install_os = template.os_name
 
         install_location = template.install_location
-        install_type = self.tdlobj.installtype
-        install_script = parameters['install_script']
+        install_type = template.installtype
+        install_script = parameters.get('install_script')
         install_config = {'admin_password': parameters.get('admin_password'),
                           'license_key': parameters.get('license_key'),
                           'arch': template.os_arch,
@@ -65,18 +64,23 @@ class Nova(object):
                           'storage': parameters.get('storage'),
                           'name': template.name}
 
-        self.nib = NIB(install_os,
-                           install_location,
-                           install_type,
-                           install_script,
-                           install_config)
-        #TODO: this needs to be changed to return some reference to the base_image that was created (sloranz)
+        self.nib = NIB(install_os, install_location, install_type, install_script, install_config)
         self.nib.run()
-        self.nib.wait_for_completion(180)
+
+        os_image_id = self.nib.wait_for_completion(180)
+        if os_image_id:
+            #TODO: set this somewhere else on the image, otherwise we mismatch the id with names/paths for the pim
+            builder.base_image.identifier = os_image_id
+
 
     def create_target_image(self, builder, target, base_image, parameters):
         #self.log.info('create_target_image() called for Nova plugin - creating a TargetImage')
-        self.log.info('create_target_image() currently unsupported')
+        self.log.info('create_target_image() called for Nova plugin - creating TargetImage')
 
     def add_cloud_plugin_content(self, content):
         self.log.info('add_cloud_plugin_content() currently unsupported')
+
+    def _cached_image(self, os_image_id):
+        #TODO: check for previously downloaded image, open it, and return the file object
+        image_file = StackEnvironment().download_image_from_glance(os_image_id)
+        return image_file
